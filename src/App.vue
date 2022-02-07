3b6d7dfbc5d4c977a420052b66c0e377b9fa23a5
PabloPalomino <82877357+PabloPalomino@users.noreply.github.com>
2022-02-07 14:19:22 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+ <!--  <img alt="Vue logo" src="./assets/logo.png"> -->
+  <HelloWorld msg="Mitos de la NBA" autor="Pablo"/>
+<formulario-persona @add-persona="agregarPersona" />
+
+<tabla-personas :personas="personas" />
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TablaPersonas from './components/TablaPersonas.vue'
+import FormularioPersona from './components/FormularioPersona.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TablaPersonas,
+    FormularioPersona
+  }, 
+  data(){
+    return{
+      personas:[
+        {
+          id:"100",
+          nombre:"Michael Jordan",
+          equipo:"Chicago bulls",
+          correo:"michael@gmail.com"
+          },
+          {
+          id:"200",
+          nombre:"Bryan Scoot",
+          equipo:"LA Lakers",
+          correo:"bryan@gmail.com"
+          },
+          {
+          id:"300",
+          nombre:"Kevin Mchale",
+          equipo:"Celtics Boston",
+          correo:"kevin@gmail.com"
+          }
+
+      ]//cierra array personas
+
+      
+
+      }//cierra return
+    },//cierra data
+methods: {
+
+    agregarPersona(persona) {
+
+    let id = 0;
+
+if (this.personas.length > 0) {
+
+id = this.personas[this.personas.length - 1].id + 1;
+
+}
+
+this.personas= [...this.personas, { ...persona, id}];
+
+}
+
+}
+    
+}//cierra default
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FormularioPersona.vue
@@ -0,0 +1,107 @@
+<template>
+
+<div id="formulario-persona">
+
+<form @submit.prevent="enviarFormulario">
+
+<div class="container">
+
+<div class="row">
+
+
+
+<div class="col-md-4">
+
+
+<div class="form-group">
+
+<label>Jugador</label>
+
+<input v-model="persona.nombre" type="text" class="form-control" />
+
+</div>
+
+</div>
+
+<div class="col-md-4">
+
+<div class="form-group">
+
+<label>Equipo</label>
+
+<input v-model="persona.equipo" type="text" class="form-control" />
+
+</div>
+
+</div>
+
+<div class="col-md-4">
+
+<div class="form-group">
+
+<label>Email</label>
+
+<input v-model="persona.correo" type="email" class="form-control" />
+
+</div>
+
+</div>
+
+</div>
+
+<div class="row">
+
+<div class="col-md-4">
+
+<div class="form-group">
+
+<button class="btn btn-primary">Añadir persona</button
+>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+</form>
+
+</div>
+
+</template>
+
+
+<script>
+export default {
+
+name: 'formulario-persona',
+
+data() {
+
+return {
+
+    persona: {
+        id: '',
+        jugador: '',
+        equipo: '',
+        correo:'',
+},
+}
+
+},
+
+    methods: {
+
+    enviarFormulario() {
+
+        this.$emit('add-persona', this.persona);
+
+    }
+
+},
+
+
+}
+</script>
